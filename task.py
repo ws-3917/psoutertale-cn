95014429be55f69bad06844f50046f6773bea601
ws-3917 <ws3917@outlook.com>
2024-10-13 09:14:57 +0800

    2024-10-13 09:14:57 Update translation. \nAuthor: ws3917

--- task.py
+++ task.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     #bashcmd("export all_proxy=127.0.0.1:7897")  # 保证代理顺畅#
     #convert_s2t("assets/values-zh_CN.ts", "assets/values-zh_TW.ts")
     task_update()
-    #task_release(55.4, "百分之六十")
+    task_release(55.4, "百分之六十")
     #task_showdiff(53.9, 54, lang='zh_CN', output="losttext.txt")
     #task_dttvl_copyfiles()
     #task_dttvl_update(update_lang='zh_CN')
