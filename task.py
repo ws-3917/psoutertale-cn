fca4f249add94bcdb8427eefe9149cee1b08aeef
ws-3917 <ws3917@outlook.com>
2024-11-02 12:52:28 +0800

    4

--- task.py
+++ task.py
@@ -3,8 +3,8 @@
 if __name__ == "__main__":
     # bashcmd("export all_proxy=127.0.0.1:7897")  # 保证代理顺畅#
     # convert_s2t("assets/values-zh_CN.ts", "assets/values-zh_TW.ts")
-    task_update()
-    # task_release(55.5, "中立4-21")
+    # task_update()
+    task_release(55.5, "中立4-21")
     # task_showdiff(53.9, 54, lang='zh_CN', output="losttext.txt")
     # task_dttvl_copyfiles()
     # task_dttvl_update(update_lang="zh_CN")
